66313acb7d0d1265b74df53e3c69a9b176cf9a0c
Pander <pander@users.sourceforge.net>
2022-05-17 10:26:38 +0200

    fixed inclusion filters Audacity

--- scripts/6-zip.sh
+++ scripts/6-zip.sh
@@ -1,5 +1,5 @@
 cd ../generated
 for i in $(ls *-fit-response.tsv|sort -n); do
     i=$(basename $i -fit-response.tsv)
-    zip $i.zip $i*tsv $i*.xml $i*.png
+    zip $i.zip $i*tsv $i*.txt $i*.png
 done
